22d1adcc26193419b697559352830fa97dc43d06
Mehmet_Emre Yılmaz <mehmetemreyilmaz199991@gmail.com>
2024-04-03 19:24:36 +0300

    add checkUser link and control user information

--- routes/users.js
+++ routes/users.js
@@ -26,6 +26,22 @@ router.post("/", async(req,res)=>{
 })
 
 router.post("/checkUser" , async(req,res)=>{
+    const userName = req.body.userName
+    const password = req.body.password
+    console.log(userName,password)
+    const user = await User.findOne({ userName: userName, password: password });
+    if (user) {
+        const userData={
+            userName:user.userName,
+            mail:user.mail,
+            phone:user.phone
+        }
+        console.log(userData)
+        res.send (userData)
+    } else {
+        res.send (null)
+        // Kullanıcı bulunamadı, hata döndür
+    }
 
     //req.body içinde userName ve password gelecek 
     //Bunları db de kontrol et böyle biri var mı diye kulllanıdı varsa tüm bilgilerini sini döndür yoksa null döndür hata fırlatma db çöker
